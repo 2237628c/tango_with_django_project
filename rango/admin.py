d8d489bf5944358e6a8b3c2afc71b9b8da5d7115
Adam Christie <2237628c@student.gla.ac.uk>
2018-02-03 00:28:09 +0000

    chapter5.7

--- rango/admin.py
+++ rango/admin.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from rango.models import Category, Page
+
+
+admin.site.register(Category)
+admin.site.register(Page)
